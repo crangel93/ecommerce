62dde68ed5c9fc1560e34fbf8e4c17c296d6c375
Christian Rangel <budrangel@gmail.com>
2022-10-03 11:25:31 -0700

    pushing models

---
+++ models/index.js
@@ -0,0 +1,41 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  // as: 'product_tags',
+  foreignKey: 'product_id'
+});
+
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  // as: 'product_tags',
+  foreignKey: 'tag_id'
+});
+
+
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
